801f72bbae399c13d91e98aee6a7bb0ec83c4155
TamerPW <tamer666@wp.pl>
2025-10-24 22:43:38 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,21 +1,36 @@
-    public:
-        Wektor2D() //konstruktor
-        {
-            x=0; 
-            y=0; 
-            cout << "współrzędne wektora: " << "x = " << x << " y = " << y << endl;
-        } 
-
-        Wektor2D(double a, double b) 
-        {
-            x=a; 
-            y=b; 
-            cout << "współrzędne wektora: " << "x = " << x << " y = " << y << endl;
-        }; 
-
-        ~Wektor2D() //destruktor
-        {
-            cout << "Zniszczenie" << endl;
-@@ -62,4 +60,3 @@
-    scalar =v1.getX()*v2.getX()+ v1.getY()*v2.getY(); 
+#include <math.h>
+class Wektor2D
+{
+public:
+    Wektor2D()
+    {
+        x = 0;
+        y = 0;
+    };
+    Wektor2D(double a, double b)
+    {
+        x = a;
+        y = b;
+    };
+    ~Wektor2D()
+    {};
+    void setX(int a) { x = a; };
+    void setY(int b) { y = b; };
+    int getX() { return x; };
+    int getY() { return y; };
+    double x;
+    double y;
+};
+Wektor2D operator+(Wektor2D v1, Wektor2D v2)
+{
+    double sumx, sumy;
+    sumx = v1.getX() + v2.getX();
+    sumy = v1.getY() + v2.getY();
+    return Wektor2D{sumx, sumy};
+};
+double operator*(Wektor2D v1, Wektor2D v2)
+{
+    double scalar;
+    scalar = v1.getX() * v2.getX() + v1.getY() * v2.getY();
     return scalar;
+};
